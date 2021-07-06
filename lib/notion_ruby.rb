72c0b9b126475b480e75809a9ee2b4b1556bc978
Akifumi Tominaga <tominaga.switch@gmail.com>
2021-07-06 22:36:59 +0900

    fix library api

--- lib/notion_ruby.rb
+++ lib/notion_ruby.rb
@@ -5,14 +5,20 @@ require_relative "notion_ruby/connection"
 require_relative "notion_ruby/errors"
 require_relative "notion_ruby/state_methods"
 require_relative "notion_ruby/resource_proxy"
+require_relative "notion_ruby/api/blocks"
 require_relative "notion_ruby/api/databases"
 require_relative "notion_ruby/api/pages"
+require_relative "notion_ruby/api/search"
+require_relative "notion_ruby/api/users"
 
 class NotionRuby
   attr_reader :connection
 
+  include NotionRuby::API::Blocks
   include NotionRuby::API::Databases
   include NotionRuby::API::Pages
+  include NotionRuby::API::Search
+  include NotionRuby::API::Users
 
   def initialize(options = {}, &block)
     @options = options
