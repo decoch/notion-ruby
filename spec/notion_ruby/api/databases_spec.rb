b4bbe4c1579f08f04184fa2598b34dd2ae5a0c5f
Akifumi Tominaga <tominaga.switch@gmail.com>
2021-07-11 21:03:41 +0900

    add tests of user create and update

--- spec/notion_ruby/api/databases_spec.rb
+++ spec/notion_ruby/api/databases_spec.rb
@@ -4,21 +4,19 @@ RSpec.describe NotionRuby::API::Databases do
   let(:notion) { NotionRuby.new(access_token: NOTION_TOKEN) }
   let(:id) { "30f8638c-7bac-bd1c-62bf-81ce1fdb8069" }
 
-  context "databases" do
-    it "return a database", vcr: { cassette_name: "database" } do
-      databases = notion.databases(id)
-      expect(databases["object"]).to eql "database"
-    end
+  it "return a database", vcr: { cassette_name: "database" } do
+    databases = notion.databases(id)
+    expect(databases["object"]).to eql "database"
+  end
 
-    it "return databases", vcr: { cassette_name: "databases" } do
-      databases = notion.databases
-      expect(databases["object"]).to eql "list"
-    end
+  it "return databases", vcr: { cassette_name: "databases" } do
+    databases = notion.databases
+    expect(databases["object"]).to eql "list"
+  end
 
-    it "return filtered pages in a database", vcr: { cassette_name: "database_query" } do
-      query = { filter: { or: [{ property: "title", text: { contains: "A Notion Page" } }] } }
-      databases = notion.databases(id).query(query)
-      expect(databases["object"]).to eql "list"
-    end
+  it "return filtered pages in a database", vcr: { cassette_name: "database_query" } do
+    query = { filter: { or: [{ property: "title", text: { contains: "A Notion Page" } }] } }
+    databases = notion.databases(id).query(query)
+    expect(databases["object"]).to eql "list"
   end
 end
--- spec/notion_ruby/api/pages_spec.rb
+++ spec/notion_ruby/api/pages_spec.rb
@@ -3,11 +3,50 @@
 RSpec.describe NotionRuby::API::Pages do
   let(:notion) { NotionRuby.new(access_token: NOTION_TOKEN) }
   let(:id) { "3c088cf2-c27d-9b57-156f-53ae10195c95" }
+  let(:create_params) do
+    {
+      parent: { database_id: "30f8638c-7bac-bd1c-62bf-81ce1fdb8069" },
+      properties: {
+        Name: {
+          title: [
+            {
+              text: {
+                content: "Tuscan Kale"
+              }
+            }
+          ]
+        }
+      }
+    }
+  end
+  let(:update_params) do
+    {
+      properties: {
+        Name: {
+          title: [
+            {
+              text: {
+                content: "Updated"
+              }
+            }
+          ]
+        }
+      }
+    }
+  end
 
-  context "pages" do
-    it "return a page", vcr: { cassette_name: "page" } do
-      databases = notion.pages(id)
-      expect(databases["object"]).to eql "page"
-    end
+  it "return a page", vcr: { cassette_name: "page" } do
+    pages = notion.pages(id)
+    expect(pages["object"]).to eql "page"
+  end
+
+  it "create a page", vcr: { cassette_name: "page_create" } do
+    page = notion.pages.create(create_params)
+    expect(page["object"]).to eql "page"
+  end
+
+  it "update a page", vcr: { cassette_name: "page_update" } do
+    databases = notion.pages(id).update(update_params)
+    expect(databases["object"]).to eql "page"
   end
 end
